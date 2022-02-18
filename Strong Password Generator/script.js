be388bc0af5d8b004268fd89d3fc362fb6937020
yuri.exe <77060124+mahdixabid@users.noreply.github.com>
2022-02-18 20:08:13 +0100

    script

---
+++ Strong Password Generator/script.js
@@ -0,0 +1,55 @@
+let alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "0123456789";
+let symbols = "!@#$%^&*_-+=";
+let passwordTxt = document.getElementById("password");
+let length = document.getElementById("length");
+let NumberData = document.getElementById("numbers");
+let SymbolsData = document.getElementById("symbols");
+let generateBtn = document.getElementById("generate");
+
+
+generateBtn.onclick = function() {
+    let characters = alpha;
+    if (NumberData.checked) {
+        characters += numbers
+    }
+    if (SymbolsData.checked) {
+        characters += symbols
+    }
+    passwordTxt.value = generatePassword(length.value, characters);
+};
+
+
+// function sizeOflenght() {
+//     if (characters.length>20)
+//     {
+//         document.
+//     }
+// }
+
+
+function generatePassword(length, characters) {
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(
+            (Math.random() * characters.length)
+        );
+    }
+    return password;
+};
+
+
+// Copy Output
+
+function passwordResult() {
+    let copyText = document.getElementById("password");
+    copyText.select();
+    if (copyText.value == "")
+        alert("Output is Empty")
+    else {
+        navigator.clipboard.writeText(copyText.value);
+        alert("Password Copied To Clipboard");
+    }
+
+
+}
