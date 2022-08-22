6aa84afefb35cb6520d501f8beb61c74b23cbd74
yuri.exe <mahdiabid158@gmail.com>
2022-08-22 15:51:23 +0100

    update

--- To-Do_App/main.js
+++ To-Do_App/main.js
@@ -1,15 +1,11 @@
 let input = document.getElementById("input")
 let add = document.getElementById("add")
 let taskDiv = document.getElementById("tasks")
-
 var ArrayData = []
-if(window.localStorage.getItem('tasks')) {
+if (window.localStorage.getItem('tasks')) {
     var ArrayData = JSON.parse(localStorage.getItem('tasks'));
 }
-
 showData()
-
-
 add.addEventListener('click', function () {
     if (input.value != "") {
         NewTask(input.value);
@@ -17,6 +13,15 @@ add.addEventListener('click', function () {
     }
     else {
         alert("input is empty")
+    }
+})
+// Update & Delete
+taskDiv.addEventListener('click', function (e) {
+    // Delete element from localstorage
+    deleteData(e.target.parentElement.getAttribute("data-id"));
+    // Delete element from the page 
+    if (e.target.classList.contains("del")) {
+        e.target.parentElement.remove()
     }
 })
 
@@ -39,6 +44,9 @@ function AddTasks(e) {
     e.forEach(function (task) {
         let Element = document.createElement("div")
         Element.className = 'task';
+        if (task.completed) {
+            Element.className = "task done"
+        }
         Element.setAttribute("data-id", task.id)
         Element.textContent = task.title
         taskDiv.appendChild(Element)
@@ -51,10 +59,21 @@ function AddTasks(e) {
 function saveData(ArrayData) {
     window.localStorage.setItem("tasks", JSON.stringify(ArrayData))
 }
- function showData() {
-     let savedData = window.localStorage.getItem('tasks');
-     if (savedData) {
-         let tasks = JSON.parse(savedData);
-         AddTasks(tasks);
-     }
- }
+function showData() {
+    let savedData = window.localStorage.getItem('tasks');
+    if (savedData) {
+        let tasks = JSON.parse(savedData);
+        AddTasks(tasks);
+    }
+}
+
+function deleteData(elementID) {
+    // checking ids of elements
+    // for (let i = 0; i < ArrayData.length; i++) {
+    //     if (el === storageData) {
+    //         console.log(storageData)
+    //     }
+    // }
+    ArrayData = ArrayData.filter((task) => task.id != elementID);
+    saveData(ArrayData)
+}
